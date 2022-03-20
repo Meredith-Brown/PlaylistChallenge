a9a3552c819f01111a4395ef6cea2676be5a4ee1
Meredith <meredith@Zipcoders-MacBook-Pro-2.local>
2022-03-19 21:01:10 -0400

    lab complete

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,5 +1,7 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
 public class Music {
 
     private String[] playList;
@@ -9,6 +11,50 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        Integer lowerCount = 0;
+
+        // calculate forward
+        ArrayList<String> forwardList = new ArrayList<String>();
+        for (int i = startIndex; i < playList.length; i++) {
+            forwardList.add(playList[i]);
+        }
+        for (int l = 0; l < startIndex; l++) {
+            forwardList.add(playList[l]);
+        }
+        Integer forwardCount = calculateCount(forwardList, selection);
+
+        // calculate backward
+        ArrayList<String> backwardList = new ArrayList<String>();
+        for (int k = startIndex; k >= 0; k--) {
+            backwardList.add(playList[k]);
+        }
+        for (int m = backwardList.size() - 1; m > startIndex; m--) {
+            backwardList.add(playList[m]);
+        }
+        Integer backwardCount = calculateCount(backwardList, selection);
+
+        // assess which count is lower
+        if (forwardCount < backwardCount) {
+            lowerCount = forwardCount;
+        } else if (forwardCount > backwardCount) {
+            lowerCount = backwardCount;
+        } else if (forwardCount == backwardCount) {
+            lowerCount = forwardCount;
+        }
+
+        return lowerCount;
+    }
+
+    public Integer calculateCount(ArrayList<String> arrayList, String selectionToSearchFor) {
+        int count = 0;
+        for (int j = 0; j < arrayList.size(); j++) {
+            String toCompare = arrayList.get(j);
+            if (toCompare.equals(selectionToSearchFor)) {
+                break;
+            } else {
+                count += 1;
+            }
+        }
+        return count;
     }
 }
